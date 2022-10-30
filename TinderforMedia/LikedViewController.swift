d87df034dd9c6e122c712115aec79fe05a8135b8
L.J <100539328+harmichimo@users.noreply.github.com>
2022-10-30 15:31:57 -0400

    updated likedviewcontroller

--- TinderforMedia/LikedViewController.swift
+++ TinderforMedia/LikedViewController.swift
@@ -9,24 +9,47 @@ import UIKit
 import Parse
 
 class LikedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var movieIds = [Int: Int]()
-    let myQuery = PFQuery(className:"Liked")
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var media = [PFObject]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movieIds.count
+        return media.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LikedViewCell") as! LikedViewCell
+        let data = media[indexPath.row]
+        let id = data["mediaId"] as! Int
+        print(id)
+        cell.medianameLabel.text = String(id)
         return cell
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Liked")
+        query.limit = 20
+        query.findObjectsInBackground { (liked, error) in
+            if liked != nil {
+                self.media = liked!
+                self.tableView.reloadData()
+            } else {
+                print("Error in database.")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
+    
+    
     
 
     /*
@@ -40,3 +63,4 @@ class LikedViewController: UIViewController, UITableViewDelegate, UITableViewDat
     */
 
 }
+
